3bd3497325c326ebfde62cf3659ca98198095aad
BuildTools <jh.2220@hotmail.com>
2020-02-24 15:18:34 -0300

    1.2 | Fix missin background wen start a new chat

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,16 @@
 let urlpage 	= "web.whatsapp.com";
-var img     	= "";
+let img     	= "";
 let classname   = "_1_keJ";
 let loading;
 let amoutDiv;
 let Cookie;
 
-//Load Cache
-
+/*_______________________________________________________
+[+] Load if exist a local image save in you local Storage
+_________________________________________________________*/
 if (localStorage.getItem("whatsappbg") == null){
 
 	localStorage.setItem("whatsappbg","empty");
-	img = "";
-
-}else if(localStorage.getItem("whatsappbg") == "empty"){
-
 	img = "";
 
 }else {
@@ -21,12 +18,19 @@ if (localStorage.getItem("whatsappbg") == null){
 	img = localStorage.getItem("whatsappbg");
 
 }
+/*_______________________________________________________
+[-] Load if exist a local image save in you local Storage
+_________________________________________________________*/
 
-//Load clicks background
+
+/*____________________________________________________________
+[+] The check was loaded with the elements of the application.
+______________________________________________________________*/
 function checkIsLoad(){
 
 	loading = setInterval(function(){
 	 
+	 	//Calls the main functions when whats app is logged.
 		amoutDiv = document.getElementsByClassName("X7YrQ").length;
 
 		if (amoutDiv > 0){
@@ -41,22 +45,20 @@ function checkIsLoad(){
 
 }
 
-checkIsLoad();
+document.onload = checkIsLoad();
+/*____________________________________________________________
+[-] The check was loaded with the elements of the application.
+______________________________________________________________*/
 
+/*_______________________
+[+] Click event functions
+_________________________*/
 function loadDivsElements(){
 
-	//Definindo informacoes para ser buscadas
-	let clickElement = document.getElementsByClassName("X7YrQ");
+	//When user click in anythig... background is reloaded
+	document.addEventListener("click", function(){reloadBackground(img)});
 
-	//Change Tab Whats app
-	for (let i = 0; i < clickElement.length; i++) {	
-
-		clickElement[i].addEventListener("click", function(){reloadBackground(img)});
-
-	}
-
-
-	//Exit Whats app
+	//Exit Whats app - Clear the background set for last user
 	document.getElementsByClassName('_3j8Pd')[0].addEventListener("click", function(){
 
 		try{
@@ -73,26 +75,45 @@ function loadDivsElements(){
 
 		}catch (err){};
 
-
 	});
 
 }
+/*_______________________
+[-] Click event functions
+_________________________*/
 
-//Trocando o BackGround quando clicado
+/*_______________________________________________
+[+] Event responsible for changing the background
+_________________________________________________*/
 function reloadBackground(newImageUrl){
+
+	/*[!] Every time you change the convert from whats app. 
+	There is an event of deletion of the "div" and the re-creation, 
+	so it is for this reason that it checks whether the inserted background is the same.*/
 
 	try {
 		
-		document.getElementsByClassName(classname)[0].style.background 				= "url(\"" + newImageUrl + "\")";
-		document.getElementsByClassName(classname)[0].style.repeate					= "no-repeat";
-		document.getElementsByClassName(classname)[0].style.backgroundSize 			= "cover";
-		document.getElementsByClassName(classname)[0].style.backgroundAttachment 	= "fixed";
-		document.getElementsByClassName(classname)[0].style.animation 				= "none";
+		if (newImageUrl != document.getElementsByClassName(classname)[0].style.background ) {
+
+			document.getElementsByClassName(classname)[0].style.background 						= "url(\"" + newImageUrl + "\")";
+			document.getElementsByClassName(classname)[0].style.repeate								= "no-repeat";
+			document.getElementsByClassName(classname)[0].style.backgroundSize 				= "cover";
+			document.getElementsByClassName(classname)[0].style.backgroundAttachment 	= "fixed";
+			document.getElementsByClassName(classname)[0].style.animation							= "none";
+
+		}
 
 	} catch(err){};
 
 }
+/*_______________________________________________
+[-] Event responsible for changing the background
+_________________________________________________*/
 
+
+/*_______________________________________________________________
+[+] "CHROME" API event to receive the background exchange request
+_________________________________________________________________*/
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse){
 
 	img = request['data'];
@@ -105,12 +126,13 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse){
 
 	}else{
 
-		console.log("clear background");
-
-		localStorage.setItem("whatsappbg","null");
+		localStorage.removeItem("whatsappbg");
 
 		reloadBackground("");
 
 	}
 
 });
+/*_______________________________________________________________
+[-] "CHROME" API event to receive the background exchange request
+_________________________________________________________________*/
